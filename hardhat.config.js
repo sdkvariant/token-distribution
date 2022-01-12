d1071cc3ef20110bcfcd86e99718b24cd0352e45
chefking <chefking9000@protonmail.com>
2022-01-12 21:04:39 +0100

    function: hexTonumber

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,3 +2,9 @@ require('dotenv').config();
 const data = require('./data.json');
 const ethers = require('ethers');
 const { cyan, gray, green } = require('chalk');
+
+function hexToNumber(hex) {
+  return parseFloat(ethers.utils.formatEther(
+    ethers.BigNumber.from(hex)
+  ));
+}
