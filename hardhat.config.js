726a36a23ed7d90f3447837e5cfe74cb03643226
chefking <chefking9000@protonmail.com>
2022-01-12 22:40:44 +0100

    Log summary on each iteration

--- hardhat.config.js
+++ hardhat.config.js
@@ -31,4 +31,53 @@ task('unicheck', 'Checks if an account/s can claim UNI rewards')
       if (accounts === ',') {
         accounts = Object.keys(data.claims).join(',');
       }
+
+      accounts = accounts.split(',');
+    console.log(gray(`  > Checking ${accounts.length} accounts...`));
+    console.log(gray(`  > Using provider: ${providerUrl ? providerUrl : 'Ethers fallback provider'}`));
+    console.log('\n');
+
+    let toBeClaimed = hasBeenClaimed = canClaim = haveClaimed = 0;
+    const num = accounts.length;
+    for (let i = 0; i < num; i++) {
+      const account = ethers.utils.getAddress(accounts[i]);
+      console.log(cyan(`Checking account ${i} of ${num} ${account}`));
+
+      const entry = data.claims[account];
+      if (entry) {
+        const amount = hexToNumber(entry.amount);
+
+        console.log(gray(`  > Account *could* claim ${amount} UNI...`));
+        // console.log(entry);
+
+        console.log(gray(`  > Querying TokenDistributor.isClaimed(${entry.index})...`));
+        const TokenDistributor = new ethers.Contract(
+          '0x090d4613473dee047c3f2706764f49e0821d256e',
+          require('./abis/TokenDistributor.json'),
+          provider
+        );
+
+        const isClaimed = await TokenDistributor.isClaimed(entry.index);
+        if (isClaimed) {
+          console.log(gray(`  > Account has already claimed ${amount} UNI`));
+
+          haveClaimed++;
+          hasBeenClaimed += amount;
+        } else {
+          console.log(green(`  > Account has ${amount} UNI to claim!`));
+
+          canClaim++;
+          toBeClaimed += amount;
+        }
+      } else {
+        console.log(gray('  > Account has no UNI to claim :('));
+      }
+      console.log(gray(`  > ${canClaim} accounts can claim ${toBeClaimed} UNI`));
+      console.log(gray(`  > ${haveClaimed} accounts have claimed ${hasBeenClaimed} UNI`));
+    }
+  });
+
+  module.exports = {
+    solidity: '0.7.3',
+  };
       
